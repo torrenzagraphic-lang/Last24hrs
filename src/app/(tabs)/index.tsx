2e11641775e3a940c1e53f83ff7985aab9ef15c6
torrenzagraphic-lang <torrenzagraphic@gmil.com>
2026-03-13 17:26:38 +0530

    Fix errors

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
-import { Button } from '@expo/ui/jetpack-compose';
+// import { Button } from '@expo/ui/jetpack-compose';
 import { Link, useRouter } from "expo-router";
-import { ActivityIndicator, StyleSheet, Text, TextInput, View } from "react-native";
+import { ActivityIndicator, Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
@@ -11,9 +11,10 @@ export default function Index() {
       <TextInput placeholder="Emails"/>
       <ActivityIndicator size={"large"}/>
       <Link href={'/about'}>Go about</Link>
-      <Button onPress={() => router.push('/about')}>
+      {/* <Button onPress={() => router.push('/about')}>
         <Text>Button</Text>
-      </Button>
+      </Button> */}
+      <Button title="Go about" onPress={() => router.push('/about')}/>
     </View>
   );
 }
