7c026fab4e14e7eec69f71f70f4a8300972d4e4c
torrenzagraphic-lang <torrenzagraphic@gmil.com>
2026-04-03 14:03:22 +0530

    Complete JSON Filles

--- src/lib/supabase/Client.ts
+++ src/lib/supabase/Client.ts
@@ -1,16 +1,19 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { createClient } from '@supabase/supabase-js';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+import Constants from "expo-constants";
 
+const supabaseUrl = Constants.expoConfig?.extra?.supabaseUrl;
+const supabaseAnonKey = Constants.expoConfig?.extra?.supabaseAnonKey;
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error("Supabase config missing 🚨");
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-    auth:{
+    auth: {
         storage: AsyncStorage,
         autoRefreshToken: true,
         persistSession: true,
         detectSessionInUrl: false,
     },
 });
-
-
